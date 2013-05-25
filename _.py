4d2df09b0c5581ebc1bc78a15254466856f3034f
Grom PE <i@grompe.org.ru>
2013-05-25 09:13:19 +0700

    Build reorganization to keep build number off git

--- _.py
+++ _.py
@@ -1,27 +1,27 @@
 import time
-import re
 import sys
 import os
 
-VERSION_PATTERN = 'VERSION_TEXT = ".*build (\d+)"'
-
 def increaseBuildVersion():
-  def upd(matchobj):
-    newbuild = int(matchobj.group(1))+1
-    today = time.strftime("%d %b %Y")
-    print "build %d is cooking..."%(newbuild)
-    return 'VERSION_TEXT = "%s, build %d"'%(today, newbuild)
-
-  newdata = re.sub(VERSION_PATTERN, upd, data)
-
-  f = open("mod_moreEverything/source/mod_moreEverything.java", "w")
-  f.write(newdata)
-  f.close()
-
   global buildnumber
   buildnumber += 1
 
+  f = open("mod_moreEverything.java", "rb")
+  data = f.read()
+  f.close()
+
+  newdata = data.replace("@version@", "%s, build %d"%(time.strftime("%d %b %Y"), buildnumber))
+
+  f = open("buildnumber", "wb")
+  f.write(str(buildnumber))
+  f.close()
+
+  f = open("mod_moreEverything/source/mod_moreEverything.java", "wb")
+  f.write(newdata)
+  f.close()
+
 def compileAndPack():
+  os.system(r"copy *.java mod_moreEverything\source\ /y >nul")
   increaseBuildVersion()
   errcode = os.system("javac -source 1.6 -target 1.6 -bootclasspath c:/things/jre6u26/lib/rt.jar -d mod_moreEverything/ -Xlint:unchecked -cp deps_modloader/*;deps_common/* mod_moreEverything/source/*.java")
   if errcode == 0:
@@ -35,15 +35,14 @@ def pack():
   print "Wrote %s"%(zipfile)
 
 if __name__ == "__main__":
-  f = open("mod_moreEverything/source/mod_moreEverything.java", "r")
-  data = f.read()
-  f.close()
-
-  buildnumber = int(re.search(VERSION_PATTERN, data).group(1));
-
   if len(sys.argv) < 2:
     print "Usage: _ compile | pack"
     exit()
+
+  f = open("buildnumber", "rb")
+  buildnumber = int(f.read())
+  f.close()
+
   if sys.argv[1] == "compile":
     compileAndPack()
   elif sys.argv[1] == "pack":
